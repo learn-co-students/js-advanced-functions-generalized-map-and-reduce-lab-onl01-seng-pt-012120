81c7dee8e5d7c6e4531f26baf0ef3f9eb1a2e70b
Jonathan <Jonnyghan@gmail.com>
2020-08-27 14:40:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, call) {
+    let a = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let elem =array[i]
+      a.push(call(elem))
+    }
+  
+    return a;
+  }
+  
+  function reduce(array, call, start){
+    let a = (!!start ? start : array[0])
+    let i = (!!start ? 0 : 1)
+  
+    for (; i < array.length; i++) {
+      a = call(array[i], a)
+    }
+  
+    return a;
+  }
