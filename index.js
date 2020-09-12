ddbfba8d7beae7150851f3d8f4120ef387e22173
Daniel <danielcarvalhovr@hotmail.com>
2020-09-12 11:49:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+//map receives an array and a block of code as parameters
+//then set a variable storing an empty array
+//next we set an loop system to iterate over every element in the array
+function map(src, cb) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(cb(theElement))
+    }
+  
+    return r;
+  }
+  
+  //reduce takes an array, a block of code, and a starting point
+  function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
